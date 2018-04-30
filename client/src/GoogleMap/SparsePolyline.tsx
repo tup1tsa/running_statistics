34bc47d6683113f175839bfb02f551de9a54f724
tup1tsa <tup1tsa@rambler.ru>
2018-04-30 15:11:00 +0300

    add sparse polyline

---
+++ client/src/GoogleMap/SparsePolyline.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Polyline } from 'react-google-maps';
+import { Position } from '../Path/PathFetcher';
+
+interface PathPart {
+  positions: Position[];
+  color: string;
+}
+
+interface Props {
+  path: PathPart[];
+}
+
+export const SparsePolyline = (props: Props) => {
+  const polylines = props.path.
+    map((pathPart, pathPartNumber) => {
+      const positions = pathPart.positions.map(position => {
+        return {
+          lat: position.latitude,
+          lng: position.longitude
+        };
+      });
+      return <Polyline path={positions} key={pathPartNumber} options={{strokeColor: pathPart.color}} />;
+    });
+  return <>{polylines}</>;
+};
