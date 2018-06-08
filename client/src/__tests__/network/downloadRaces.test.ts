10c9ee34c15b45f4e5761df62b0e855102dea996
tup1tsa <tup1tsa@rambler.ru>
2018-06-08 11:03:53 +0300

    download races from server logic

---
+++ client/src/__tests__/network/downloadRaces.test.ts
@@ -0,0 +1,58 @@
+import { downloadRaces } from '../../network/downloadRaces';
+import { Response } from '../../common_files/interfaces';
+
+describe('download races logic', () => {
+
+  const successResponse: Response = {
+    data: [],
+    status: 200
+  };
+
+  it('should call correct api endpoint', async (done) => {
+    const axios = {
+      get: jest.fn(),
+      post: jest.fn().mockResolvedValue(successResponse)
+    };
+    await downloadRaces(axios, jest.fn());
+    expect(axios.post.mock.calls.length).toBe(1);
+    expect(axios.post.mock.calls[0][0]).toBe('/fetchRaces');
+    done();
+  });
+
+  it('should throw an error if server responded with an error', async (done) => {
+    const badStatus: Response = {
+      status: 404
+    };
+    const wrongData: Response = {
+      status: 200,
+      data: 'invalid data'
+    };
+    const axios = {
+      get: jest.fn(),
+      post: jest.fn()
+        .mockResolvedValueOnce(badStatus)
+        .mockResolvedValueOnce(wrongData)
+    };
+    await expect(downloadRaces(axios, jest.fn())).rejects.toEqual(new Error('server is not available'));
+    await expect(downloadRaces(axios, jest.fn())).rejects.toEqual(new Error('data is invalid'));
+    done();
+  });
+
+  it('should return only valid races', async (done) => {
+    const response: Response = {
+      data: [{path: 'correct'}, {path: 'incorrect'}],
+      status: 200
+    };
+    const axios = {
+      get: jest.fn(),
+      post: jest.fn().mockResolvedValueOnce(response)
+    };
+    const validatePath = jest.fn()
+      .mockReturnValueOnce(true)
+      .mockReturnValue(false);
+    const races = await downloadRaces(axios, validatePath);
+    expect(races).toEqual([{path: 'correct'}]);
+    done();
+  });
+
+});
