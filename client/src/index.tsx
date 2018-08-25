6649611af306673d2349c4485b695ac24b283881
unknown <natorvano.work@gmail.com>
2018-08-25 11:47:59 +0300

    polyfills for es5 browsers

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,6 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './application/App';
