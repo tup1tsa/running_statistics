ab33d00ceca1ba69989cffe2246baa46d619f317
tup1tsa <natorvano@gmail.com>
2019-02-20 18:16:30 +0200

    fix unique token ts error

--- src/__tests__/generateUniqueAccessToken.test.ts
+++ src/__tests__/generateUniqueAccessToken.test.ts
@@ -1,14 +1,20 @@
 import { generateUniqueAccessTokenFactory } from "../application/generateUniqueAccessToken";
+import { IsAccessTokenUnique } from "../application/database/queries/isAccessTokenUnique";
 
 it("should generate unique access token", async done => {
-  const isAccessTokenUnique = jest
+  // there is weird bug with ts types
+  // .mockResolveValueOnce(true) throws an error
+  // todo: investigate it later
+  const isAccessTokenUnique: IsAccessTokenUnique = jest
     .fn()
-    .mockResolvedValueOnce(false)
-    .mockResolvedValueOnce(true);
+    .mockReturnValueOnce(Promise.resolve(false))
+    .mockReturnValueOnce(Promise.resolve(false))
+    .mockReturnValueOnce(Promise.resolve(true));
   const createSalt = jest
     .fn()
     .mockReturnValueOnce("hey")
-    .mockReturnValueOnce("way");
+    .mockReturnValueOnce("smin")
+    .mockReturnValue("way");
   const token = await generateUniqueAccessTokenFactory(
     createSalt,
     isAccessTokenUnique
