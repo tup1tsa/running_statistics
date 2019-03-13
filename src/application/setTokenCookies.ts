ad9ddc390e84e8690393415fc3b07728514f9ad9
tup1tsa <natorvano@gmail.com>
2019-03-13 14:59:01 +0200

    fix cookie response binding

--- src/application/setTokenCookies.ts
+++ src/application/setTokenCookies.ts
@@ -6,11 +6,12 @@ interface User {
   readonly accessToken: string;
 }
 
-export const setTokenCookies: SetTokenCookies = ({ locals, cookie }) => {
-  const user: User = locals.user;
+export const setTokenCookies: SetTokenCookies = response => {
+  const user: User = response.locals.user;
   if (!user) {
     throw new Error("user should be set in locals already");
   }
+  const cookie = response.cookie.bind(response);
   cookie("accessToken", user.accessToken, {
     maxAge: 30 * 24 * 60 * 60 * 1000
   });
