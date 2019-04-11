d8035c30cbb4955a2045c28dd68004acf17d1581
tup1tsa <natorvano@gmail.com>
2019-04-11 17:27:13 +0300

    races validation client side

--- client/src/__tests__/logic/network/downloadRaces.test.ts
+++ client/src/__tests__/logic/network/downloadRaces.test.ts
@@ -2,7 +2,7 @@ import { MESSAGES } from "running_app_core";
 import { downloadRacesFactory } from "../../../application/logic/network/downloadRaces";
 
 const successResponse = {
-  data: [],
+  data: { races: [] },
   status: 200
 };
 
@@ -30,7 +30,7 @@ it("should throw an error if server responded with an error", async done => {
 
 it("should throw error if data is invalid", async done => {
   const response = {
-    data: [{ path: "bad data" }],
+    data: { races: [{ path: "bad data" }] },
     status: 200
   };
   const networkRequest = jest.fn().mockResolvedValue(response);
@@ -40,18 +40,30 @@ it("should throw error if data is invalid", async done => {
     downloadRacesFactory(networkRequest, validateRaces)()
   ).rejects.toEqual(new Error(MESSAGES.racesAreNotValid));
   expect(validateRaces.mock.calls.length).toBe(1);
-  expect(validateRaces.mock.calls[0][0]).toEqual(response.data);
+  expect(validateRaces.mock.calls[0][0]).toEqual(response.data.races);
   done();
 });
 
 it("should return all races if validation is successful", async done => {
   const response = {
-    data: [{ type: "running", path: [] }],
+    data: { races: [{ type: "running", path: [] }] },
     status: 200
   };
   const networkRequest = jest.fn().mockResolvedValue(response);
   const races = await downloadRacesFactory(networkRequest, validator)();
 
-  expect(races).toEqual(response.data);
+  expect(races).toEqual(response.data.races);
+  done();
+});
+
+it("should throw an error if response type is invalid", async done => {
+  const response = {
+    data: { somethindElse: [{ type: "running", path: [] }] },
+    status: 200
+  };
+  const networkRequest = jest.fn().mockResolvedValue(response);
+  await expect(
+    downloadRacesFactory(networkRequest, validator)()
+  ).rejects.toEqual(new Error(MESSAGES.racesAreNotValid));
   done();
 });
--- client/src/application/logic/network/downloadRaces.ts
+++ client/src/application/logic/network/downloadRaces.ts
@@ -1,6 +1,10 @@
 import { MESSAGES, Race, ValidateRaces, validateRaces } from "running_app_core";
 import { NetworkRequest, networkRequest } from "./networkRequest";
 
+interface Races {
+  races?: object[];
+}
+
 export type DownloadRaces = () => Promise<ReadonlyArray<Race>>;
 type DownloadRacesFactory = (
   networkRequest: NetworkRequest,
@@ -15,10 +19,21 @@ export const downloadRacesFactory: DownloadRacesFactory = (
   if (result.errorMessage) {
     throw new Error(result.errorMessage);
   }
-  if (!validateRacesFunc(result.data)) {
-    throw new Error(MESSAGES.racesAreNotValid);
+  // cast is used because typescript for some reason doesn't understand next:
+  // if (typeof result.data === 'object' && result.data && result.data.races) {
+  //   here races should be valid value
+  //   but typescript response is 'races is not valid key of result.data'
+  // }
+  const racesObj = result.data as Races;
+  if (
+    typeof racesObj === "object" &&
+    racesObj &&
+    racesObj.races &&
+    validateRacesFunc(racesObj.races)
+  ) {
+    return racesObj.races;
   }
-  return result.data;
+  throw new Error(MESSAGES.racesAreNotValid);
 };
 
 export const downloadRaces: DownloadRaces = downloadRacesFactory(
